cdcc3c634569a56f01514c16ea6f2c5c64507e94
team awesome <maxj@email.arizona.edu>
2019-05-27 13:41:28 -0700

    form bkgrnd, code for conf. message does not work

--- assets/app.js
+++ assets/app.js
@@ -4,7 +4,29 @@ $('.popover-dismiss').popover({
     trigger: 'focus'
 });
 
-
 $(function() {
     $('[data-toggle="popover"]').popover()
+});
+
+// Javascript for display message for email form, ideally this would display after the email has been sent, in place of the JSON object that appears on the following page. 
+
+//0. add Ajax cdn
+
+//1. send message (#send_Email)
+
+//2. new page (#feedback)
+
+//3. display confirmation meassage for 7-10 seconds. (#name) for the 'name value from the form
+
+//4. redirect back to 'contact' page. 
+
+//5. form data has been cleared out. 
+
+$('send_Email').submit(function(e) {
+    e.preventDefault();
+    $.post('send_Email', function(data) {
+        var name = $('#name').val();
+        $('#feedback').html('Thanks! ' + name + ' your message to Max M. Jackson, M.Ed. has been submitted directly to him. Max usually returns his messages witin 24-48 business hours.');
+    });
+    return false;
 });
